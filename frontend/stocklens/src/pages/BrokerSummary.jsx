3a4859cf8f9bd04c7a90928cc2c68c84ace4aafe
yogeee12 <yogeshlohar129@gmail.com>
2026-07-30 17:44:24 +0530

    Updated broker summary tables page

--- frontend/stocklens/src/pages/BrokerSummary.jsx
+++ frontend/stocklens/src/pages/BrokerSummary.jsx
@@ -26,6 +26,10 @@ function BrokerSummary(){
                     <th>Postitive</th>
                     <th>Negative</th>
                     <th>Target met</th>
+                    <th>Expired</th>
+                    <th>Pre-Bonus/Split</th>
+                    <th>Postive %</th>
+                    <th>Success %</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -37,6 +41,10 @@ function BrokerSummary(){
                             <td>{broker.company_in_positive}</td>
                             <td>{broker.company_in_negative}</td>
                             <td>{broker.target_met}</td>
+                            <td>{broker.expired}</td>
+                            <td>{broker.bonus_split}</td>
+                            <td>%{broker.positive_ratio}</td>
+                            <td>%{broker.success_ratio}</td>
                         </tr>
                     ))}
                 </tbody>
