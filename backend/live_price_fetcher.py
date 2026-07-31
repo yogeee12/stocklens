e94f1ca855f0ed4ec72f689c3b2372b35c420aa8
yogeee12 <yogeshlohar129@gmail.com>
2026-07-31 12:36:44 +0530

    Added live price fetcher time consuming 3min

---
+++ backend/live_price_fetcher.py
@@ -0,0 +1,43 @@
+import yfinance as yf 
+from model import Company, Recommendation
+from database import SessionLocal
+import time
+
+def data_gen():
+    start = time.perf_counter()
+    db = SessionLocal()
+
+    companies = db.query(Company).all()
+    # symbol = [company.symbol+".NS" for company in companies]
+
+
+    for company in companies:
+        recommendation = (
+            db.query(Recommendation)
+            .filter(Recommendation.company_id == company.id)
+            .all()
+        )
+
+        ticker = yf.Ticker(company.symbol)
+        latest_price = ticker.fast_info["lastPrice"]
+
+        print("Updating Data of",company.id)
+        for reco in recommendation:
+
+            target_price =  reco.target_price if reco.target_price else None
+            price_at_reco = reco.price_at_reco if reco.price_at_reco else None
+            
+            reco.current_price =  round(latest_price,2)
+            if target_price:
+                reco.upside = round(((target_price - latest_price) / latest_price)*100,2)
+            if price_at_reco:
+                reco.change_at_reco = round(((latest_price - price_at_reco) / price_at_reco)*100,2)
+
+    db.commit()
+    end = time.perf_counter()
+    execution_time = end - start
+    print(f"Execution Time : {execution_time:.6f}")
+    db.close()
+    return "Price Changed"
+
+data_gen()
