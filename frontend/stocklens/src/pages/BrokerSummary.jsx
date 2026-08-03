46a8ca075972b0e436648001ac425b3ca1b0b495
yogeee12 <yogeshlohar129@gmail.com>
2026-08-03 11:25:44 +0530

    Redesign broker page added brokersidebar

--- frontend/stocklens/src/pages/BrokerSummary.jsx
+++ frontend/stocklens/src/pages/BrokerSummary.jsx
@@ -1,6 +1,8 @@
 import { useState ,useEffect , Fragment } from "react"
 import { getBrokerSummary } from "../services/api"
 import Header from "../components/header"
+import BrokerSidebar from "../components/BrokerSidebar"
+
 
 function BrokerSummary(){
     const [brokers, SetBrokers] = useState([])
@@ -21,83 +23,7 @@ function BrokerSummary(){
     return(
         <div>
             <Header />
-            <h1>Broker Summary</h1>
-
-            <table>
-                <thead> 
-                    <tr>
-                    <th>Broker</th>
-                    <th>Total recommendations</th>
-                    <th>Active</th>
-                    <th>Postitive</th>
-                    <th>Negative</th>
-                    <th>Target met</th>
-                    <th>Expired</th>
-                    <th>Pre-Bonus/Split</th>
-                    <th>Postive %</th>
-                    <th>Success %</th>
-                    <th>Last Recommendation Date</th>
-                    </tr>
-                </thead>
-                <tbody >
-                    {sortedBrokers.map(broker => (
-                    <Fragment key={broker.broker_id}>
-                        <tr onClick={() =>
-                            setSelectedBroker(
-                                selectedBroker === broker.broker_id ? null : broker.broker_id
-                            )
-                        }>
-                            <td>{broker.broker_name}</td>
-                            <td>{broker.total_recommendations}</td>
-                            <td>{broker.active_recommendations}</td>
-                            <td>{broker.company_in_positive}</td>
-                            <td>{broker.company_in_negative}</td>
-                            <td>{broker.target_met}</td>
-                            <td>{broker.expired}</td>
-                            <td>{broker.bonus_split}</td>
-                            <td>%{broker.positive_ratio}</td>
-                            <td>%{broker.success_ratio}</td>
-                            <td>{broker.last_recommendation_date || "-"}</td>
-                            <td>{selectedBroker === broker.broker_id ? "▲" : "▼"}</td>
-                        </tr>
-                        {selectedBroker === broker.broker_id &&
-                        <>
-                          <tr>
-                          <td colSpan="12">
-                          <table>
-                          <thead>
-                            <tr>
-                                <th>Company</th>
-                                <th>Call Type</th>
-                                <th>Current Price</th>
-                                <th>Target Price</th>   
-                                <th>Upside</th>
-                                <th>Change Since Reco</th>
-                                <th>Reco Date</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                          {broker.company_list.map((company, index) =>
-                            <tr key={index}>
-                            <td>{company.company_name}</td>
-                            <td>{company.call_type}</td>
-                            <td>₹{company.current_price}</td>
-                            <td>₹{company.target_price}</td>
-                            <td>{company.upside}%</td>
-                            <td>{company.change_at_reco}%</td>
-                            <td>{company.recommendation_date}</td>
-                            </tr>
-                        )}
-                        </tbody>
-                        </table>
-                        </td>
-                        </tr>
-                        </>
-                    }
-                    </Fragment>
-                ))}
-                </tbody >
-            </table>
+            <BrokerSidebar onSelectBroker={setSelectedBroker} />
         </div>
     )
 }
