ff17884759566a699df344f7b052b7fca050103c
yogeee12 <yogeshlohar129@gmail.com>
2026-08-04 17:58:36 +0530

    Added filter on box

--- frontend/stocklens/src/components/BrokerProfile.jsx
+++ frontend/stocklens/src/components/BrokerProfile.jsx
@@ -1,4 +1,34 @@
+import { useState } from "react"
+
 function BrokerProfile({ broker }){
+
+    const [filter, setFilter] = useState("ACTIVE")
+
+    const rows = broker.recommendation.filter(row => {
+        switch(filter){
+            
+            case "ACTIVE":
+                return row.upside !== null
+            
+            case "POSITIVE":
+                return row.upside > 0
+
+            case "NEGATIVE":
+                return row.upside < 0
+
+            case "TARGET_MET":
+                return row.upside_status === "Target met"
+
+            case "BONUS_SPLIT":
+                return row.upside_status === "Pre-Bonus/Split"
+
+            case "EXPIRED":
+                return row.upside_status === ""
+
+            default:
+                return true
+        }
+    })
 
     if(!broker){
         return <p>Loading...</p>
@@ -12,15 +42,15 @@ function BrokerProfile({ broker }){
                         <small className="broker-box-title">Total Reommendation</small>
                         <p className="broker-box-value">{broker.total_recommendations}</p>
                         </div>
-                    <div className="broker-box active_recommendations">
+                    <div className="broker-box active_recommendations " onClick={setFilter('ACTIVE')}>
                         <small className="broker-box-title">Active Recommendations</small>
                         <p className="broker-box-value">{broker.active_recommendations}</p>
                         </div>
-                    <div className="broker-box company-in-positive">
+                    <div className="broker-box company-in-positive" onClick={'POSITIVE'}>
                         <small className="broker-box-title">Positive</small>
                         <p className="broker-box-value">{broker.company_in_positive}</p>
                         </div>
-                    <div className="broker-box company-in-negative">
+                    <div className="broker-box company-in-negative" onClick={'NEGATIVE'}>
                         <small className="broker-box-title">Negative</small>
                         <p className="broker-box-value">{broker.company_in_negative}</p>
                         </div>
@@ -32,15 +62,15 @@ function BrokerProfile({ broker }){
                         <small className="broker-box-title">Positive Ratio</small>
                         <p className="broker-box-value">{broker.positive_ratio}</p>
                         </div>
-                    <div className="broker-box target-met">
+                    <div className="broker-box target-met" onClick={'TARGET_MET'}>
                         <small className="broker-box-title">Target Met</small>
                         <p className="broker-box-value">{broker.target_met}</p>
                         </div>
-                    <div className="broker-box pre-bonus-split">
+                    <div className="broker-box pre-bonus-split" onClick={'BONUS_SPLIT'}>
                         <small className="broker-box-title">Pre-Bonus/Split</small>
                         <p className="broker-box-value">{broker.bonus_split}</p>
                         </div>
-                    <div className="broker-box expired">
+                    <div className="broker-box expired" onClick={'EXPIRED'}>
                         <small className="broker-box-title">Expired</small>
                         <p className="broker-box-value">{broker.expired}</p>
                         </div>
@@ -59,7 +89,7 @@ function BrokerProfile({ broker }){
                                 </tr>
                             </thead>
                             <tbody>
-                                {broker.company_list.map(company => 
+                                {rows.map(company => 
                                 <tr>
                                     <td className="company-name">{company.company_name}</td>
                                     <td>{company.call_type}</td>
